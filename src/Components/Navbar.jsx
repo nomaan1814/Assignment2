c9109ed611004fc74e0469ff9aac0dc84f9f15d6
nomaan1814 <mohdnomaan181149@gmail.com>
2022-08-20 16:23:43 +0530

    commit assignment2

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,9 +8,7 @@ const Navbar = () => {
     <a class="flex title-font font-bold items-center text-white mb-4 md:mb-0">
       <span class="ml-3 text-xl">Github Search</span>
     </a>
-    <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a class="mr-5 hover:text-white">First Link</a>
-    </nav>
+   
   </div>
 </header>
     </div>
